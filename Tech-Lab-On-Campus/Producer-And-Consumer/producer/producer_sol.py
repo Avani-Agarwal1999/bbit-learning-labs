6e3009f583c3cef25d24cddf62afcd725e8b7a3d
Avani Agarwal <avaniagarwal@Avanis-MacBook-Air.local>
2024-02-27 18:51:56 -0600

    Spacing changes

--- Tech-Lab-On-Campus/Producer-And-Consumer/producer/producer_sol.py
+++ Tech-Lab-On-Campus/Producer-And-Consumer/producer/producer_sol.py
@@ -1,41 +1,39 @@
-import pika
 import os
+
+import pika
 from producer_interface import mqProducerInterface
 
+
 class mqProducer(mqProducerInterface):
-        def __init__(self, routing_key: str, exchange_name: str) -> None:
+    def __init__(self, routing_key: str, exchange_name: str) -> None:
         # Save parameters to class variables
-                self.routing_key = routing_key
-                self.exchange_name = exchange_name
+        self.m_routing_key = routing_key
+        self.m_exchange_name = exchange_name
         # Call setupRMQConnection
-                self.setupRMQConnection()              
-        pass
+        self.setupRMQConnection()
 
-        def setupRMQConnection(self) -> None:
-            # Set-up Connection to RabbitMQ service
-            con_params = pika.URLParameters(os.environ["AMQP_URL"])
-            self.connection = pika.BlockingConnection(parameters=con_params)
+    def setupRMQConnection(self) -> None:
+        # Set-up Connection to RabbitMQ service
+        con_params = pika.URLParameters(os.environ["AMQP_URL"])
+        self.m_connection = pika.BlockingConnection(parameters=con_params)
 
-            # Establish Channel
-            self.channel = self.connection.channel()
+        # Establish Channel
+        self.m_channel = self.m_connection.channel()
 
-            # Create the exchange if not already present
-            self.exchange = self.channel.exchange_declare(exchange="Exchange Name")
-            
+        # Create the exchange if not already present
+        self.m_channel.exchange_declare(self.m_exchange_name)
 
-            pass
+    def publishOrder(self, message: str) -> None:
+        # Basic Publish to Exchange
+        self.m_channel.basic_publish(
+            exchange=self.m_exchange_name,
+            routing_key=self.m_routing_key,
+            body=message,
+        )
 
-        def publishOrder(self, message: str) -> None:
-            # Basic Publish to Exchange
-            self.channel.basic_publish(
-                exchange="Exchange Name",
-                routing_key="Routing Key",
-                body="Message",
-            )
-            # Close Channel
-            self.channel.close()
+        print(" [x] Sent Orders")
 
-            # Close Connection
-            self.connection.close()
-        
-            pass
+    def __del__(self) -> None:
+        print(f"Closing RMQ connection on destruction")
+        self.m_channel.close()
+        self.m_connection.close()
